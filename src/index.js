e47f063096d28f7ab6bdc283a24b1a8ec75f4593
VladislavVik1 <Dakraman1232@ukr.net>
2025-07-07 12:15:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-
-dotenv.config();
+import { seedContactsIfEmpty } from './services/contacts.js'; // ⬅️ новий імпорт
 
 const startApp = async () => {
   await initMongoConnection();
+  await seedContactsIfEmpty(); // ⬅️ імпорт якщо немає контактів
   setupServer();
 };
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,10 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Contact } from '../models/Contact.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const getAllContacts = async () => {
   return await Contact.find();
@@ -6,4 +12,18 @@ export const getAllContacts = async () => {
 
 export const getContactById = async (id) => {
   return await Contact.findById(id);
+};
+
+export const seedContactsIfEmpty = async () => {
+  const count = await Contact.countDocuments();
+
+  if (count === 0) {
+    const filePath = path.join(__dirname, '../../data/contacts.json');
+    const data = await fs.readFile(filePath, 'utf-8');
+    const contacts = JSON.parse(data);
+    await Contact.insertMany(contacts);
+    console.log('✅ Контакти імпортовані автоматично!');
+  } else {
+    console.log('📌 Контакти вже існують, імпорт не потрібен');
+  }
 };
