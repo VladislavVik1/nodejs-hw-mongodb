a29ef668262225b3e8764931f07bb98898b1c393
VladislavVik1 <Dakraman1232@ukr.net>
2025-07-29 19:23:57 +0300

    12

--- src/schemas/contactSchemas.js
+++ src/schemas/contactSchemas.js
@@ -2,16 +2,17 @@ import Joi from "joi";
 
 export const addContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(7).max(20).required(),
+  email: Joi.string().email().optional(),
+  phoneNumber: Joi.string().min(7).max(20).required(), // 🟢 змінено
   isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid("personal", "work", "other").optional(),
+  contactType: Joi.string().valid("personal", "work", "home").optional(), // 🟢 "home" замість "other"
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
-  phone: Joi.string().min(7).max(20),
+  phoneNumber: Joi.string().min(7).max(20), // 🟢 змінено
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid("personal", "work", "other"),
+  contactType: Joi.string().valid("personal", "work", "home"), // 🟢 змінено
 }).min(1);
+
